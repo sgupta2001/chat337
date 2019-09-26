3b1dbb0494c97afa8cd6a3d421c50cc16cae320e
Stefan Marr <git@stefan-marr.de>
2019-09-26 16:21:10 +0100

    Added support to serve images from a `resources` folder
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 import Koa from 'koa';
+import Router from 'koa-router';
+import { readFileSync, existsSync } from 'fs';
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async ctx => {
-  ctx.body = "Hello World 2";
+router.get('/images/:fileName', async ctx => {
+  ctx.type = '.jpg';
+  const fileName = 'resources/' + ctx.params.fileName;
+  if (existsSync(fileName)) {
+    ctx.body = readFileSync(fileName);
+  } else {
+    ctx.status = 404;
+    ctx.body = `File "${ctx.params.fileName}" not found`;
+    ctx.type = '.txt';
+  }
 });
+
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(33333);
